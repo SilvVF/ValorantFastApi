a7792f0ba47eb61de15c4f11588c0be397f455ff
David Silver <david.silver232@gmail.com>
2023-01-10 00:30:47 -0500

    create new driver for each request fix multiple requests freezing the api

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
-options = uc.ChromeOptions()
-options.headless = True
-driver = uc.Chrome(use_subprocess=True, options=options)
 
 recent_fetch_cache = {}
 
@@ -27,22 +24,42 @@ def read_cache():
     return recent_fetch_cache
 
 
+def createDriver():
+    options = uc.ChromeOptions()
+    options.headless = True
+    return uc.Chrome(use_subprocess=True, options=options)
+
+
 # uvicorn main:app --reload
 @app.get("/player/{name}/{tagline}")
-def read_item(name: str, tagline: str, q: Union[str, None] = None):
-    if recent_fetch_cache.get(name + tagline, False):
-        return recent_fetch_cache[name + tagline]
-    driver.get('https://tracker.gg/valorant/profile/riot/' + name + '%23' + tagline + '/overview')
-    html = driver.page_source
-    target = "window.__INITIAL_STATE__ ="
-    start = html.find(target) + len(target)
-    current = html[start:]
-    json_site_data = current[:current.find("</script>")]
-    data = PlayerDataKeys(json.loads(json_site_data)['stats']['standardProfiles'][0]['segments'][0]["stats"])
-    response = {
-        "kd": data.kDRatio,
-        "kda": data.kDARatio,
-        "defKda": data.defenseKDRatio
-    }
-    recent_fetch_cache[name + tagline] = response
-    return response
+async def read_item(name: str, tagline: str, q: Union[str, None] = None):
+    url = 'https://tracker.gg/valorant/profile/riot/' + name + '%23' + tagline + '/overview'
+    if recent_fetch_cache.get(url, False):
+        return recent_fetch_cache[url]
+    driver = createDriver()
+    try:
+        driver.get(url)
+        html = driver.page_source
+        target = "window.__INITIAL_STATE__ ="
+        start = html.find(target) + len(target)
+        current = html[start:]
+        json_site_data = current[:current.find("</script>")]
+    except:
+        return {
+            "error": "unable to find player using url " + url
+        }
+    finally:
+        driver.close()
+    try:
+        data = PlayerDataKeys(json.loads(json_site_data)['stats']['standardProfiles'][0]['segments'][0]["stats"])
+        response = {
+            "kd": data.kDRatio,
+            "kda": data.kDARatio,
+            "defKda": data.defenseKDRatio
+        }
+        recent_fetch_cache[url] = response
+        return response
+    except:
+        return {
+            "error": "unable to convert response to player data"
+        }
